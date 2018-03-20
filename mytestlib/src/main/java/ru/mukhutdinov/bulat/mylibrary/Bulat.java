642c11c140bff02fa94abcb299072110da65ada0
bulatmukhutdinov <Bulatmm@yandex.ru>
2018-03-20 11:31:07 +0300

    Initial commit

---
+++ mytestlib/src/main/java/ru/mukhutdinov/bulat/mylibrary/Bulat.java
@@ -0,0 +1,11 @@
+package ru.mukhutdinov.bulat.mylibrary;
+
+/**
+ * Created by bulatmukhutdinov on 20.03.2018.
+ */
+
+public class Bulat {
+    public static String getRandomString() {
+        return "Bulat";
+    }
+}
